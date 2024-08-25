e63f50cdb753c5b4063d7d4316b3ec134a700dad
itprobsvinod <itprobs.com@gmail.com>
2024-08-25 13:11:19 +0530

    done

--- src/app/components/menuItems.tsx
+++ src/app/components/menuItems.tsx
@@ -1,77 +1,40 @@
-
-
-import { PanelMenu } from 'primereact/panelmenu';
+import { PanelMenu } from "primereact/panelmenu";
+import Link from "next/link";
 
 const MenuItems = () => {
-    const menuItems = [
-        {
-          label: 'Dashboard',
-          icon: 'pi pi-home',
-          to: '/',
-          exact: true
-        },
-        {
-          label: 'Transactions',
-          icon: 'pi pi-transactions',
-          to: '/transactions',
-        },
-        {
-          label: 'Accounts',
-          icon: 'pi pi-accounts'
-        },
-        {
-          label: 'Investments',
-          icon: 'pi pi-investments'
-        },
-        {
-          label: 'Credit Cards',
-          icon: 'pi pi-creditCard'
-        },
-        {
-          label: 'Loans',
-          icon: 'pi pi-loans',
-          to: '/loans'
-        },
-        {
-          label: 'Services',
-          icon: 'pi pi-services'
-        },
-        {
-          label: 'My Privileges',
-          icon: 'pi pi-privileges'
-        },
-        {
-          label: 'Setting',
-          icon: 'pi pi-setting'
-        },
-        // Uncomment this part if you want to add sub-menu items
-        /*
-        {
-          label: 'Devices',
-          icon: 'pi pi-desktop',
-          items: [
-            {
-              label: 'Phone',
-              icon: 'pi pi-mobile'
-            },
-            {
-              label: 'Desktop',
-              icon: 'pi pi-desktop'
-            },
-            {
-              label: 'Tablet',
-              icon: 'pi pi-tablet'
-            }
-          ]
-        }
-        */
-      ];
+  const menuItems = [
+    { label: "Dashboard", icon: "pi pi-home", url: "/dashboard", exact: true },
+    { label: "Transactions", icon: "pi pi-transactions", url: "/transactions" },
+    { label: "Accounts", icon: "pi pi-accounts" },
+    { label: "Investments", icon: "pi pi-investments" },
+    { label: "Credit Cards", icon: "pi pi-creditCard" },
+    { label: "Loans", icon: "pi pi-loans", url: "/loans" },
+    { label: "Services", icon: "pi pi-services" },
+    { label: "My Privileges", icon: "pi pi-privileges" },
+    { label: "Setting", icon: "pi pi-setting" },
+  ];
 
   return (
-    <nav className="main_menu" >
-              <PanelMenu model={menuItems} />   
-                </nav>
-   
+    <nav className="main_menu">
+      <ul>
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            {item.url && (
+              <Link href={item.url} className="nav_link">
+                 <span className='p-menuitem-icon' ><i className={`pi ${item.icon}`} ></i></span>
+                <span className="p-menuitem-text" >{item.label}</span>
+              </Link>
+            )}
+            {!item.url && (
+              <span className="nav_link">
+                <span className='p-menuitem-icon' ><i className={`pi ${item.icon}`} ></i></span>
+                <span className="p-menuitem-text" >{item.label}</span>
+              </span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-   <h1>Login</h1>
+   <main>
+    <h1>Login</h1>
+   </main>
   );
 }
